d2f625baac36019b63b9afa1c876bbd5d1ca1f3f
conor <conorlyness@gmail.com>
2022-10-10 19:30:40 +0100

    track what tab so the content can be lazily loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  tabIndex = 0;
   constructor() {}
   ngOnInit(): void {}
+
+  changeTab(event: any) {
+    console.log(event.index);
+    this.tabIndex = event.index;
+  }
 }
