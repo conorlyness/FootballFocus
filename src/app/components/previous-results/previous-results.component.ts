8e0056fd4bfc74db8e7351646658665eb447fcc8
Conor Lyness <Conorlyness@gmail.com>
2022-11-07 21:50:31 +0000

    previous result enhancements

--- src/app/components/previous-results/previous-results.component.ts
+++ src/app/components/previous-results/previous-results.component.ts
@@ -9,16 +9,21 @@ import { ApiService } from 'src/app/services/services/api.service';
 export class PreviousResultsComponent implements OnInit {
   @Input() league!: string;
   fixtureData: any[] = [];
+  previousResultsData: any[] = [];
+  currentResultsData: any[] = [];
   prem: boolean = false;
   serieA: boolean = false;
   laLiga: boolean = false;
   bundes: boolean = false;
   ligue1: boolean = false;
   loading: boolean = false;
+  currentGameweek: any;
+  showingPreviousWeeks: boolean = false;
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
     this.loading = true;
+
     if (this.league === 'prem') {
       this.prem = true;
     } else if (this.league === 'serieA') {
@@ -30,6 +35,22 @@ export class PreviousResultsComponent implements OnInit {
     } else {
       this.ligue1 = true;
     }
+
+    this.api
+      .getCurrentFixtureRound(
+        this.prem,
+        this.serieA,
+        this.laLiga,
+        this.bundes,
+        this.ligue1
+      )
+      .subscribe((gWeek: any) => {
+        var gameweek = gWeek.match(/\d/g);
+        gameweek = gameweek.join('');
+        this.currentGameweek = gameweek;
+        console.log('Current gameweek ', this.currentGameweek);
+      });
+
     this.api
       .getLeagueResults(
         this.prem,
@@ -41,9 +62,26 @@ export class PreviousResultsComponent implements OnInit {
       .subscribe((data: any) => {
         this.loading = false;
         const fixturesObj = data[0];
+
+        this.currentResultsData = fixturesObj[Object.keys(fixturesObj)[0]];
+        this.previousResultsData = fixturesObj[Object.keys(fixturesObj)[1]];
         //gets the first object in the object
-        this.fixtureData = fixturesObj[Object.keys(fixturesObj)[0]];
+        this.fixtureData = this.currentResultsData;
         console.log('league fixtures', this.fixtureData);
       });
+  }
+
+  //shows previous game weeks results
+  showPreviousResults() {
+    this.fixtureData = this.previousResultsData;
+    this.currentGameweek = this.currentGameweek - 1;
+    this.showingPreviousWeeks = true;
+  }
+
+  //shows current game weeks results
+  showCurrentResults() {
+    this.fixtureData = this.currentResultsData;
+    this.currentGameweek = this.currentGameweek + 1;
+    this.showingPreviousWeeks = false;
   }
 }
