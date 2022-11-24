05b7e1c389fca889518018f93cb425bb84b5a8a2
Conor Lyness <Conorlyness@gmail.com>
2022-11-24 20:06:28 +0000

    inital work on getting further team details

--- src/app/components/team-stats-dialog/team-stats-dialog.component.ts
+++ src/app/components/team-stats-dialog/team-stats-dialog.component.ts
@@ -7,6 +7,8 @@ import {
 import { DialogData } from '../league-table/league-table.component';
 import { TeamStats } from 'src/app/types';
 import { OrdinalPipe } from 'src/app/pipes/ordinal.pipe';
+import { ApiService } from 'src/app/services/services/api.service';
+import { forkJoin, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-team-stats-dialog',
@@ -16,10 +18,16 @@ import { OrdinalPipe } from 'src/app/pipes/ordinal.pipe';
 export class TeamStatsDialogComponent implements OnInit {
   teamStats!: TeamStats;
   teamForm: string[] = [];
+  teamID!: number;
+  detailedTeamStats!: any;
+  detailedPlayerStats!: any;
+  loading: boolean = false;
+  subscriptions = new Subscription();
 
   constructor(
     public dialogRef: MatDialogRef<TeamStatsDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    public api: ApiService
   ) {
     this.teamStats = this.data.stats;
   }
@@ -27,9 +35,44 @@ export class TeamStatsDialogComponent implements OnInit {
   ngOnInit(): void {
     let formString = this.teamStats?.form;
     this.teamForm = formString?.split('');
+    this.teamID = this.teamStats?.team.id;
+
+    console.log(this.teamStats);
+  }
+
+  ngAfterContentInit() {
+    this.getMoreDetails(this.teamID);
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  getMoreDetails(id: number) {
+    this.loading = true;
+    console.log('called get more details with id of: ', id);
+    let teamDetails = this.api.getTeamsDetails(id);
+    let playerDetails = this.api.getTeamsPlayers(id);
+
+    let teamAndPlayers = forkJoin({
+      team: teamDetails,
+      players: playerDetails,
+    });
+
+    this.subscriptions.add(
+      teamAndPlayers.subscribe({
+        next: (value) => {
+          const { team } = value as any;
+          const { players } = value as any;
+
+          this.detailedTeamStats = team;
+          this.detailedPlayerStats = players;
+          console.log(this.detailedTeamStats);
+          console.log(this.detailedPlayerStats);
+          this.loading = false;
+        },
+        error: (err) => console.log('got an error: ', err),
+      })
+    );
   }
 }
