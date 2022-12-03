ba66ab6ac7e54ac6000e5eaf5274943c08814ba5
Conor Lyness <Conorlyness@gmail.com>
2022-12-03 12:43:13 +0000

    update package scripts to run playwright commands

--- tests/tabs.spec.ts
+++ tests/tabs.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+//locators
+const premierLeagueText = '.premierLeagueTab';
+const laLigaText = '.laLigaTab';
+const bundesligaText = '.bundesligaTab';
+const serieAText = '.serieATab';
+const ligue1Text = '.ligue1Tab';
+const highlightsText = '.highlightsTab';
+
 test.describe('navigation', () => {
   test.beforeEach(async ({ page }) => {
     // Go to football focus before the tests run
@@ -9,17 +17,16 @@ test.describe('navigation', () => {
   test('Football focus has correct text on tabs for all 5 major leagues', async ({
     page,
   }) => {
-    //locators
-    const premierLeagueText = page.locator('.premierLeagueTab');
-    const laLigaText = page.locator('.laLigaTab');
-    const bundesligaText = page.locator('.bundesligaTab');
-    const serieAText = page.locator('.serieATab');
-    const ligue1Text = page.locator('.ligue1Tab');
+    await expect
+      .soft(page.locator(premierLeagueText))
+      .toHaveText('Premier League');
+    await expect.soft(page.locator(laLigaText)).toHaveText('La Liga');
+    await expect.soft(page.locator(bundesligaText)).toHaveText('Bundesliga');
+    await expect.soft(page.locator(serieAText)).toHaveText('Serie A');
+    await expect.soft(page.locator(ligue1Text)).toHaveText('Ligue 1');
+  });
 
-    await expect.soft(premierLeagueText).toHaveText('Premier League');
-    await expect.soft(laLigaText).toHaveText('La Liga');
-    await expect.soft(bundesligaText).toHaveText('Bundesliga');
-    await expect.soft(serieAText).toHaveText('Serie A');
-    await expect.soft(ligue1Text).toHaveText('Ligue 1');
+  test('Football focus has correct text on highlight tab', async ({ page }) => {
+    await expect(page.locator(highlightsText)).toHaveText('Highlights');
   });
 });
