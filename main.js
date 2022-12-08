2520b24f7a560b0a62cb7798feef8abc8836a6c7
Conor Lyness <Conorlyness@gmail.com>
2022-12-08 21:35:12 +0000

    remove menu and change app icon

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const { app, BrowserWindow } = require("electron");
 async function createWindow() {
   win = new BrowserWindow({
     show: false,
-    icon: "./src/assets/football.ico",
+    icon: "./src/assets/app-logo.ico",
     webPreferences: {
       webSecurity: false,
     },
   });
+  win.removeMenu();
   win.maximize();
   win.loadFile("./dist/football-focus-app/index.html");
 }
