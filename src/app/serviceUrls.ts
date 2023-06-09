ebdc175365037f6f10c2b7a48f48eee788b2c440
Conor Lyness <conorlyness@gmail.com>
2023-06-09 21:02:07 +0100

    updates now season is over

--- src/app/serviceUrls.ts
+++ src/app/serviceUrls.ts
@@ -13,10 +13,10 @@ export let urls = {
   },
   leagueRound: {
     //current round url
-    premierLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=39&season=${currentSeason}&current=true`,
-    serieALeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=135&season=${currentSeason}&current=true`,
-    laLigaLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=140&season=${currentSeason}&current=true`,
-    bundesligaLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=78&season=${currentSeason}&current=true`,
-    ligue1LeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=61&season=${currentSeason}&current=true`,
+    premierLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=39&season=${currentSeason}&current=false`,
+    serieALeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=135&season=${currentSeason}&current=false`,
+    laLigaLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=140&season=${currentSeason}&current=false`,
+    bundesligaLeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=78&season=${currentSeason}&current=false`,
+    ligue1LeagueCurrentRoundUrl: `https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=61&season=${currentSeason}&current=false`,
   },
 };
